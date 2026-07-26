570ef8d09816a5ee4f6016fb8440ee08b27c7944
admin <111@q.s>
2026-07-26 22:10:14 +0800

    deleted:    "\346\226\260\345\273\272\346\226\207\344\273\266\345\244\271/h.py" 	deleted:    "\346\226\260\345\273\272\346\226\207\344\273\266\345\244\271/n.py" 	deleted:    "\346\226\260\345\273\272\346\226\207\344\273\266\345\244\271/n.txt"

--- 新建文件夹/n.py
+++
@@ -1,7 +0,0 @@
-a= open("app.py","r",encoding="utf-8").read()
-b =open("a.html","r",encoding="utf-8").read()
-c = open("tool/u1.py","r",encoding="utf-8").read()
-d = open("tool/u2.py","r",encoding="utf-8").read()
-n = open("n.txt",'w',encoding="utf-8")
-
-print(f"[a.py]\n{a}\n\n\n[a.html]\n{b}\n\n\n[u1.py]\n{c}\n\n[u2.py]\n{d}\n\n请修复zip e",file=n)
