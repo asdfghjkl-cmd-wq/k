a6471c2485d93cc249f004aa34a23225d52423d6
admin <111@q.s>
2026-08-13 18:35:08 +0800

    yhhtfjhngv

--- f/app.py
+++ f/app.py
@@ -1733,7 +1733,7 @@ if __name__ == '__main__':
         sm = random.randint(6000,6050)
         if not is_port_in_use(sm):
             break
-
+    r.set('man_port',sm)
     print(f"管理端口链接:{socket.gethostbyname(socket.gethostname())}:{sm}",flush=True)
     logging.info(f"管理端口链接:{socket.gethostbyname(socket.gethostname())}:{sm}")
     s = Thread(target=w, daemon=True,args=(sm,))
@@ -1749,7 +1749,7 @@ else:
                   # 你最终强制使用的端口，建议直接固定
         print(f"管理端口链接: {socket.gethostbyname(socket.gethostname())}:{sm}", flush=True)
         logging.info(f"管理端口链接: {socket.gethostbyname(socket.gethostname())}:{sm}")
-
+        r.set('man_port',sm)
         s = Thread(target=w, daemon=True, args=(sm,))
         s.start()
         atexit.register(ss)
