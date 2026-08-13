53765685761a2627704cbd0d03f88f3b92b7e13f
admin <111@q.s>
2026-08-13 16:40:23 +0800

    fi

--- f/app.py
+++ f/app.py
@@ -56,7 +56,7 @@ import redis
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 REDIS_DB = int(os.environ.get('REDIS_DB', 0))
-REDIS_PASSWORD  = int(os.environ.get('REDIS_PASSWORD', ""))
+REDIS_PASSWORD  = os.environ.get('REDIS_PASSWORD', "")
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD,decode_responses=True)
 print(r.info('server')['redis_version'],flush=True)
